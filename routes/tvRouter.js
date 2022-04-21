35e843e859e57fc586ae1c8b82d3f9c9984087cc
Louise Findlay <louise@louisefindlay.com>
2022-04-21 21:27:04 +0100

    Added dislikedRecommendations function

--- routes/tvRouter.js
+++ routes/tvRouter.js
@@ -84,6 +84,92 @@ tvRouter.get("/:tv/season/:season/episode/:episode", function (req, res) {
         });
 });
 
+function dislikedRecommendations(genres) {
+    let genre = genres[0].name;
+    switch (genre) {
+        case "Action & Adventure":
+            genre = 10751;
+            console.info("Family");
+            break;
+        case "Animation":
+            genre = 10762;
+            console.info("Kids");
+            break;
+        case "Comedy":
+            genre = 18;
+            console.info("Drama");
+            break;
+        case "Crime":
+            genre = 10768;
+            console.info("War & Politics");
+            break;
+        case "Documentary":
+            genre = 10764;
+            console.info("Reality");
+            break;
+        case "Drama":
+            genre = 35;
+            console.info("Comedy");
+            break;
+        case "Family":
+            genre = 10762;
+            console.info("Kids");
+            break;
+        case "Kids":
+            genre = 16;
+            console.info("Animation");
+            break;
+        case "Mystery":
+            genre = 80;
+            console.info("Crime");
+            break;
+        case "News":
+            genre = 10763;
+            console.info("News");
+            break;
+        case "Reality":
+            genre = 10766;
+            console.info("Soap");
+            break;
+        case "Sci-Fi & Fantasy":
+            genre = 10759;
+            console.info("Action & Adventure");
+            break;
+        case "Soap":
+            genre = 10764;
+            console.info("Reality");
+            break;
+        case "Talk":
+            genre = 99;
+            console.info("Documentary");
+            break;
+        case "War & Politics":
+            genre = 10767;
+            console.info("Talk");
+            break;
+        case "Western":
+            genre = 10759;
+            console.info("Action & Adventure");
+            break;
+        default:
+            console.info(genre);
+    }
+    console.info(genre);
+    axios
+        .get(`https://api.themoviedb.org/3/discover/tv`, {
+            params: {
+                api_key: process.env.TMDB_API_KEY,
+                with_genres: genre,
+            },
+        })
+        .then(function (response) {
+            console.info(response.data.results);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
 tvRouter.get("/:id", function (req, res) {
     axios
         .get(`https://api.themoviedb.org/3/tv/${req.params.id}`, {
@@ -94,6 +180,7 @@ tvRouter.get("/:id", function (req, res) {
         })
         .then(function (response) {
             console.info(response.data);
+            dislikedRecommendations(response.data.genres);
             res.render("pages/tv/index", {
                 tv: response,
             });
